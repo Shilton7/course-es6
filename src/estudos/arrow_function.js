435b430770032aa5a91904633517d54826a73a49
Shilton <shilton.sistemas@gmail.com>
2020-02-05 13:43:32 -0300

    Code Review, axios and Config Webpack Server

--- src/estudos/arrow_function.js
+++ src/estudos/arrow_function.js
@@ -1,26 +1,28 @@
 // Arrrow Function
 
+// Simples
 const arr = [1, 3, 4, 5, 6];
 
-// Exemplo 1
 const newArrExemplo1 = arr.map(item => {
   return item * 2;
 });
 
 console.log(newArrExemplo1);
 
-//Exemplo 2 - anónima
+//Função Anônima
+const arr = [1, 3, 4, 5, 6];
+
 const newArrExemplo2 = arr.map(item => item * 2);
 console.log(newArrExemplo2);
 
-//Exemplo 3
+//Função usando const
 const teste = () => {
   return 'teste';
 };
 
 console.log(teste());
 
-//Exemplo 4 - retornado Objeto
+//Função retornando um Objeto
 const teste4 = () => ({ nome: 'Shilton' });
 
 console.log(teste4());
