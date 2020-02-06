40e6dc234120c1502badb94dc47f2cea78b83547
Shilton <shilton.sistemas@gmail.com>
2020-02-06 07:44:03 -0300

    Creating application with ES6 +

--- src/main.js
+++ src/main.js
@@ -1,14 +1,93 @@
-import axios from 'axios';
+import api from './services/api';
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
-      console.log(response);
-    } catch (err) {
-      console.warn('Erro na API');
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
+    this.lisEl = document.getElementById('repo-list');
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
     }
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) {
+      return;
+    }
+    this.setLoading();
+
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url }
+      } = response.data;
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.inputEl.value = '';
+
+      this.render();
+    } catch (err) {
+      alert('Repositorio não existe');
+    }
+    this.setLoading(false);
+  }
+
+  render() {
+    this.lisEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.lisEl.appendChild(listItemEl);
+    });
   }
 }
 
-Api.getUserInfo('shilton7');
+new App();
